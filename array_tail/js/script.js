5826f7503b98174a95e03335ede358b7bee062a3
DorinVieru <dorino.vieru@gmail.com>
2023-11-24 18:03:59 +0100

    [UPDATE] Improve code js & finish project

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,13 +1,16 @@
+// CHIEDO ALL'UTENTE QUANTI ELEMNTI DOVRà CONTENERE L'ARRAY
+let element_array = prompt("Di quanti elementi dovrà essere composto l'array?")
+
 // DICHIARO L'ARRAY VUOTO
 let num_array = [];
 console.log(num_array);
 
 // CREO IL CICLO NEL QUALE VERRANO PESCATI NUMERI RANDOM FINO AD UN MASSIMO DI 10 VOLTE
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= element_array; i++) {
     num_array.push(Math.floor(Math.random() * 100 + 1));
     console.log(num_array);
 }
 
 // STAMPO SOLO GLI ULTIMI 5 ELEMENTI DELL'ARRAY
-let new_num = num_array.slice(5,10)
+let new_num = num_array.slice(-5)
 console.log(new_num);
